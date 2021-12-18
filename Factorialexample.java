c1b3cc3082584655924bd0489983201d6235f17b
Dheeraj <dheeraj.thoke89@gmail.com>
2021-12-18 11:23:24 +0530

    Java program for Factorial

---
+++ Factorialexample.java
@@ -0,0 +1,19 @@
+package factorialexample;
+
+public class Factorialexample 
+{
+	public static int factorial(int n) 
+	{
+		int i=1;
+		int fact=1;
+		for(i=1;i<=n;i++)
+			fact = fact*i;
+		return fact;
+		
+	}
+	public static void main(String[] args) {
+		System.out.println(factorial(1));
+
+	}
+
+}
